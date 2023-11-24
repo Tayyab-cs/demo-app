11c0f85ab0ea550eed53632bf4b138c79af080c1
tayyab <muhammadtayyab.cs@gmail.com>
2023-11-24 11:27:50 -0800

    Added multiple buttons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import { useState, useCallback  } from "react";
+import { useState } from "react";
 import "./App.css";
 import { Flex, Button } from "antd";
+import { COLORS } from "./colors.js";
 
 function App() {
   const [bgColor, setBgColor] = useState('#fff');
 
-  const getRandomColor = useCallback (() => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }, []);
+  const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * COLORS.length);
+    const color = COLORS[randomIndex];
+    return color.hex;
+  };
 
   return (
-  <div style={{ backgroundColor: bgColor, height: '100vh', padding: '5px' }}>
+  <div style={{ backgroundColor: bgColor, height: '100%', padding: '5px' }}>
     <Flex gap="small" wrap="wrap">
         <Button name="default" onClick={() => setBgColor('#fff')}>Default</Button>
-        <Button name="green" type="dashed" onClick={() => setBgColor('#008000')}>Green</Button>
-        <Button name="blue" type="primary" onClick={() => setBgColor('#0000FF')}>Blue</Button>
-        <Button name="random" type="text" onClick={() => setBgColor(getRandomColor())}>Random</Button>
+        <Button name="random" type="dashed" onClick={() => setBgColor(getRandomColor())}>Random</Button>
+        <Button name="green" onClick={() => setBgColor('#008000')}>Green</Button>
+        <Button name="blue" onClick={() => setBgColor('#0000FF')}>Blue</Button>
+        {
+          COLORS.map((color, index) => (
+            <Button name={color.name} key={color.name + index} onClick={() => setBgColor(color.hex)}>
+              {color.name}
+            </Button>
+          ))
+        }
     </Flex>
   </div>
   )
