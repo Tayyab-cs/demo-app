59293a7e9e092db8697b775e13d4e5e4ca6410a4
tayyab <muhammadtayyab.cs@gmail.com>
2023-11-23 18:36:53 -0800

    added 4 buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useState, useCallback  } from "react";
 import "./App.css";
+import { Flex, Button } from "antd";
 
 function App() {
-  return <div className="App">Hello React</div>;
+  const [bgColor, setBgColor] = useState('#fff');
+
+  const getRandomColor = useCallback (() => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }, []);
+
+  return (
+  <div style={{ backgroundColor: bgColor, height: '100vh', padding: '5px' }}>
+    <Flex gap="small" wrap="wrap">
+        <Button name="default" onClick={() => setBgColor('#fff')}>Default</Button>
+        <Button name="green" type="dashed" onClick={() => setBgColor('#008000')}>Green</Button>
+        <Button name="blue" type="primary" onClick={() => setBgColor('#0000FF')}>Blue</Button>
+        <Button name="random" type="text" onClick={() => setBgColor(getRandomColor())}>Random</Button>
+    </Flex>
+  </div>
+  )
 }
 
 export default App;
