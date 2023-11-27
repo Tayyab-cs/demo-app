690704975cf5cc0e345be0f8a100781de6d5005f
Tayyab-cs <muhammadtayyab.cs@gmail.com>
2023-11-27 16:54:10 +0500

    test push

--- src/App.js
+++ src/App.js
@@ -4,27 +4,29 @@ import { COLORS } from "./colors.js";
 
 function App() {
   // Random Color Picker
-  const getRandomColor = () => {
-    const randomIndex = Math.floor(Math.random() * COLORS.length);
-    const color = COLORS[randomIndex];
-    return color.hex;
-  };
+  // const getRandomColor = () => {
+  //   const randomIndex = Math.floor(Math.random() * COLORS.length);
+  //   const color = COLORS[randomIndex];
+  //   return color.hex;
+  // };
 
-  // default and random colors array
-  const newColors = [
-    { name: "default", hex: "#ffffff" },
-    { name: "random", hex: getRandomColor },
-  ];
+  // // default and random colors array
+  // const newColors = [
+  //   { name: "default", hex: "#ffffff" },
+  //   { name: "random", hex: getRandomColor },
+  // ];
 
-  // checking newColors exists in COLORS array or not
-  newColors.forEach(newColor => {
-    const colorExists = COLORS.some(color => color.name === newColor.name);
-    if (!colorExists) {
-      COLORS.unshift(newColor);
-    }
-  });
+  // // checking newColors exists in COLORS array or not
+  // newColors.forEach(newColor => {
+  //   const colorExists = COLORS.some(color => color.name === newColor.name);
+  //   if (!colorExists) {
+  //     COLORS.unshift(newColor);
+  //   }
+  // });
 
-  return <ColorButtons colors={COLORS} />;
+  //return <ColorButtons colors={COLORS} />;
+
+  return <div>hello</div>
 }
 
 export default App;
